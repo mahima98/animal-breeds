2d6704ceacfb0c968ed061740fc78bd80e180cff
mahima98 <mahimaramgolam98@gmail.com>
2023-03-16 22:15:20 +0400

    fetch selected breed WIP

--- app.vue
+++ app.vue
@@ -8,12 +8,26 @@
       <p>Hello {{ name }} !</p>
    </div>
 
-   <div class="grid grid-cols-2 md:grid-cols-8 lg:grid-cols-12 gap-4">
-    <div v-for="(breed, id) in breeds" key="id" >
-        <button class="py-1 px-3 border flex justify-center items-center rounded-lg hover:text-gray-400 transition-all">
+   <div class="grid">
+    <div class="columns-8">
+      <div v-for="(breed, id) in breeds" :key="breed" >
+        <button class="py-1 px-3 border flex justify-center items-center rounded-lg hover:text-gray-400 transition-all" 
+        @click="currentBreed = breed"
+        >
           {{ breed }}
         </button>
+      </div>
     </div>
+
+    <div class="flex justify-center items-center">
+      <div v-if="currentBreed">
+        <img :src="`https://dog.ceo/api/breed/${currentBreed}/images/random`" :alt="currentBreed">
+      </div>
+      <div v-else>
+        <div>hi</div>
+      </div>
+    </div>
+    
    </div>
   </div>
 </template>
@@ -21,7 +35,8 @@
 <script setup>
 // from https://dog.ceo/dog-api/breeds-list
 
-const name = ref("dfg")
+const name = ref("dogs")
+const currentBreed = ref("")
 
 const {data} = await useFetch("https://dog.ceo/api/breeds/list/all")
 const breeds = Object.keys(data.value.message)
