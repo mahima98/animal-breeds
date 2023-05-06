36d4972cc4da234ebabb9b2e069cc47b8869a707
mahima98 <mahimaramgolam98@gmail.com>
2023-05-06 22:13:16 +0400

    upd design

--- components/Doggo.vue
+++ components/Doggo.vue
@@ -7,6 +7,6 @@ defineProps({
 </script>
 
 <template>
-  <img :src="image" class="max-w-[700px] max-h-[700px] object-contain transition-all rounded-xl border-teal-300 border-8 p-2"/>
+  <img :src="image" class="max-w-[700px] max-h-[500px] object-contain transition-all rounded-xl border-teal-500 border-8 p-2"/>
 </template>
 
